cfc4ad6ff8b11592eaab8c2e728898d5200bf764
xiaohangsu <sxhdragon@gmail.com>
2017-08-25 00:15:58 -0400

    Init chill

--- app/index.js
+++ app/index.js
@@ -1,4 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-require('bootstrap');
 require('./../style/scss/style.scss');
+
+
+class Index extends React.Component {
+    render() {
+        return <h1>Hello from React!</h1>;
+    }
+}
+
+ReactDOM.render(
+    <Index/>,
+    document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,17 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     module: {
-        rules: [{
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            },{
             test: /\.scss$/,
             loader: extractSass.extract({
                 use: [{
